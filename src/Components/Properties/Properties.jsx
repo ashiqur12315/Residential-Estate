e0db944e5e5a968c47e5ab0a7402b04ec6499d0f
ashiqur12315 <ashiqur12315@gmial.com>
2024-04-18 21:30:25 +0530

    Added design to property private route

--- src/Components/Properties/Properties.jsx
+++ src/Components/Properties/Properties.jsx
@@ -8,6 +8,7 @@ import { FaLocationDot } from "react-icons/fa6";
 import { Helmet } from 'react-helmet-async';
 import { GiIsland } from "react-icons/gi";
 import { GrStatusGood } from "react-icons/gr";
+import { AiFillCaretRight } from "react-icons/ai";
 
 
 const Properties = () => {
@@ -26,8 +27,8 @@ const Properties = () => {
                 <div className=" rounded-3xl bg-gradient-to-r from-black to-transparent opacity-80 min-h-screen lg:pt-5 md:pt-3 sm:pt-3 text-neutral-content">
                     <div className="w-full">
                        <h1 data-aos="fade-right" data-aos-duration="3000" className="mb-5 lg:text-7xl md:text-5xl sm:text-2xl  font-bold pl-5 flex"><FaBuilding />{estate_title}</h1>
-                        <p data-aos="fade-left" data-aos-duration="3000" className="m-3 pl-5 lg:text-3xl md:text-2xl sm:text-xl">{description}</p>
-                        <p data-aos="fade-right" data-aos-duration="3000" className="m-3 pl-5">Type: {segment_name}</p>
+                        <p data-aos="fade-left" data-aos-duration="3000" className="m-3 pl-5 lg:text-3xl md:text-2xl sm:text-xl bg-gradient-to-r from-cyan-400 to-transparent text-yellow-500 rounded-md font-medium p-2">{description}</p>
+                        <p data-aos="fade-right" data-aos-duration="3000" className="m-3 pl-5 flex items-center gap-2"><AiFillCaretRight />Type: {segment_name}</p>
                         <p data-aos="fade-left" data-aos-duration="3000" className="m-3 pl-5 flex items-center gap-2"><GiIsland />Area: {area}</p>
                         <p data-aos="fade-right" data-aos-duration="3000" className="m-3 pl-5 flex items-center gap-2"><RiPriceTagFill />Status: {status}</p>
                         <p data-aos="fade-left" data-aos-duration="3000" className="m-3 pl-5 flex items-center gap-2 italic">< IoPricetagsOutline /><span className='bg-orange-400 font-bold text-3xl p-2 rounded-md'>Price: {price}</span></p>
